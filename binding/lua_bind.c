9fb0831279f354e4a472ff1b42b924d64f2a219f
Pugnator <ookami@mail.ru>
2014-09-16 01:19:43 +0400

    added several basic functions

--- binding/lua_bind.c
+++ binding/lua_bind.c
@@ -1,15 +1,81 @@
 #include <vsm_api.h>
 
-static int l_sin (lua_State *L);
+static int32_t lua_console_log (lua_State *L);
+static int32_t lua_set_pin_state (lua_State *L);
+static int32_t lua_get_pin_state (lua_State *L);
+static int32_t lua_is_pin_low (lua_State *L);
+static int32_t lua_is_pin_high (lua_State *L);
+static int32_t lua_is_pin_floating (lua_State *L);
+static int32_t lua_out_log(lua_State *L);
+static int32_t lua_out_message(lua_State *L);
+static int32_t lua_out_warning(lua_State *L);
+static int32_t lua_out_error(lua_State *L);
+
+void register_functions (lua_State *L)
+{
+	/* console_log Lua bind */
+	lua_pushcfunction(L, lua_console_log);
+    lua_setglobal(L, "console_log");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_set_pin_state);
+    lua_setglobal(L, "set_pin_state");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_get_pin_state);
+    lua_setglobal(L, "get_pin_state");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_is_pin_low);
+    lua_setglobal(L, "is_pin_low");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_is_pin_high);
+    lua_setglobal(L, "is_pin_high");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_is_pin_floating);
+    lua_setglobal(L, "is_pin_floating");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_out_log);
+    lua_setglobal(L, "out_log");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_out_message);
+    lua_setglobal(L, "out_message");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_out_warning);
+    lua_setglobal(L, "out_warning");
+    /* Lua bind */
+    lua_pushcfunction(L, lua_out_error);
+    lua_setglobal(L, "out_error");
+   
+
+    lua_pushinteger(L, SHI);
+	lua_setglobal(L, "SHI");
+	lua_pushinteger(L, SLO);
+	lua_setglobal(L, "SLO");
+	lua_pushinteger(L, FLT);
+	lua_setglobal(L, "FLT");
+	lua_pushinteger(L, UNDEFINED);
+	lua_setglobal(L, "UNDEFINED");
+
+	for (int i=0; device_pins[i].name; i++)
+	{
+		lua_pushinteger(L, i);
+		lua_setglobal(L, device_pins[i].name);		
+	}
+}
 
 const char *lua_execute_script (const char *function)
 {	
 	/* Main state of Lua */
-	lua_pushcfunction(luactx, l_sin);
-    lua_setglobal(luactx, "mytest");
+	
 	int32_t lua_err = 0;       
     /* Load script but do not execute it */
-    lua_err = luaL_loadfile ( luactx, "c:\\test.lua" );
+    char spath[512] = {0};
+    if ( 0 == GetEnvironmentVariable("LUAVSM", spath, sizeof spath))
+    {
+    	console_log("LUAVSM env variable was not set");
+    }
+    char script[512]={0};
+    sprintf(script, "%s\\test.lua", spath);
+    
+    lua_err = luaL_loadfile ( luactx, script );
     if(0 != lua_err)
     {
     	switch(lua_err)
@@ -38,7 +104,6 @@ const char *lua_execute_script (const char *function)
     lua_getglobal(luactx, function);
     /* First argument */
     //lua_pushstring(luactx, command);
-    
     /* Execute script */
     lua_pcall(luactx, 0, LUA_MULTRET, 0);
     /* Return result */
@@ -50,9 +115,147 @@ const char *lua_execute_script (const char *function)
     return "";
 }
 
-static int l_sin (lua_State *L) 
+static int32_t lua_console_log (lua_State *L) 
 {
   const char *text = lua_tostring(L, -1);  /* get argument */  
   console_log("%s", text);
   return 0;  /* number of results */
+}
+
+static int32_t lua_set_pin_state (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (2 != argnum)
+  {
+  	console_log("Function %s expects 2 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+	return 0;  
+  }
+  int32_t pin_num = lua_tonumber(L, 1);
+  int32_t pin_state = lua_tonumber(L, -1);
+  //console_log("Pin %d, state: %d\n", pin_num, pin_state);
+  set_pin_state(device_pins[pin_num], pin_state);
+  return 0;  /* number of results */
+}
+
+static int32_t lua_get_pin_state (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  int32_t pin_num = lua_tonumber(L, -1);
+  
+  if(TRUE == is_pin_high(device_pins[pin_num].pin))
+  {
+  	lua_pushnumber(L, SHI);  
+  	return 1;
+  }
+  else if(TRUE == is_pin_low(device_pins[pin_num].pin))
+  {
+  	lua_pushnumber(L, SLO);
+  	return 1;
+  }
+  else if (TRUE == is_pin_floating(device_pins[pin_num].pin))
+  {
+  	lua_pushnumber(L, FLT);
+  	return 1;	
+  }
+  else
+  {
+  	lua_pushnumber(L, UNDEFINED);
+  	return 1;	
+  } 
+}
+
+static int32_t lua_is_pin_low (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  int32_t pin_num = lua_tonumber(L, -1);  
+  lua_pushboolean(L, is_pin_low(device_pins[pin_num].pin));
+  return 1;  
+}
+
+static int32_t lua_is_pin_high (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  int32_t pin_num = lua_tonumber(L, -1);  
+  lua_pushboolean(L, is_pin_high(device_pins[pin_num].pin));
+  return 1;  
+}
+
+static int32_t lua_is_pin_floating (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  int32_t pin_num = lua_tonumber(L, -1);  
+  lua_pushboolean(L, is_pin_floating(device_pins[pin_num].pin));
+  return 1;  
+}
+
+static int32_t lua_out_log (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  const char *text = lua_tostring(L, -1); 
+  out_log(text); 
+  return 0;  
+}
+
+static int32_t lua_out_message (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  const char *text = lua_tostring(L, -1); 
+  out_message(text); 
+  return 0;  
+}
+
+static int32_t lua_out_warning (lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  const char *text = lua_tostring(L, -1); 
+  out_warning(text); 
+  return 0;  
+}
+
+static int32_t lua_out_error(lua_State *L) 
+{  
+  int32_t argnum = lua_gettop(L);
+  if (1 != argnum)
+  {
+  	console_log("Function %s expects 1 arguments got %d\n", __PRETTY_FUNCTION__, argnum);
+  	return 0;
+  }
+  const char *text = lua_tostring(L, -1); 
+  out_error(text); 
+  return 0;  
 }
