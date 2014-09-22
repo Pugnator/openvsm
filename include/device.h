e98bca1dc67341ca60e74cdcde02b9c8c78dc2a7
Pugnator <ookami@mail.ru>
2014-09-23 01:26:24 +0400

    mode is broken

---
+++ include/device.h
@@ -0,0 +1,20 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+#include <vsm_api.h>
+
+typedef struct VSM_PIN
+{
+	BOOL is_digital;
+	char* name;
+	int8_t number;
+	ABSTIME on_time;
+	ABSTIME off_time;
+	STATE default_state;
+	IDSIMPIN* pin;
+} VSM_PIN;
+
+IINSTANCE* model_instance;
+IDSIMCKT* model_dsim;
+extern VSM_PIN device_pins[];
+void device_simulate ( void );
+#endif
