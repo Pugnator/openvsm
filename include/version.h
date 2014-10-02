2e7eb2dd86ad3aec0f3b8a3982885eebaed734a6
Pugnator <ookami@mail.ru>
2014-10-03 03:42:27 +0400

    Migrated to github

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 #define FILEVER        1,0,0,0
 #define PRODUCTVER     1,0,0,0
-#define STRFILEVER     "0.0.50\0"
-#define STRPRODUCTVER  "0.0.50\0"
+#define STRFILEVER     "0.0.53\0"
+#define STRPRODUCTVER  "0.0.53\0"
