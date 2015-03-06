72d6a967d9eae4c168161940468608546081c94a
Pugnator <ookami@mail.ru>
2015-03-06 16:53:23 +0300

    fixed relgen.sh. Now DLL version match the source version

--- relgen.sh
+++ relgen.sh
@@ -2,4 +2,7 @@
 majorversion=0
 subvn=$(git rev-list --merges HEAD | wc -l)
 subsubvn=$(git shortlog | grep -E '^[ ]+\w+' | wc -l)
+sed -e "s/{major}/$majorversion/" \
+-e "s/{minor}/$subvn/" \
+-e "s/{build}/$subsubvn/" ../include/_version.h > ../include/version.h
 echo $majorversion.$subvn.$subsubvn
