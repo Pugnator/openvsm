8a8a31bf60e124d9843ea028d3a717e04dd7060d
Pugnator <ookami@mail.ru>
2015-04-09 18:17:30 +0300

    Added the way to precompile main script

---
+++ src/device/device.lua
@@ -0,0 +1,18 @@
+-- Here you can place you device code. 
+-- It will be automatically compiled at build time
+__USE_PRECOMPILED = 0xDEADC0DE
+
+--NAND gate placeholder script
+SAFE_MODE=true
+LOGIC_TYPE=TTL
+
+device_pins =
+{
+    {is_digital=true, name = "A"},
+    {is_digital=true, name = "B"},
+    {is_digital=true, name = "Y"},
+}
+
+function device_simulate ()
+    Y:set(1-(A:get() * B:get()))
+end
