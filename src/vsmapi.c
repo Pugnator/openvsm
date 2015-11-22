4bcfbc666ff494cc8c611a7e85e86b796faf2127
Pugnator <ookami@mail.ru>
2015-11-22 03:15:56 +0300

    Set to use user script by default and if not set - use precompiled script

--- src/vsmapi.c
+++ src/vsmapi.c
@@ -168,7 +168,7 @@ vsm_setup ( IDSIMMODEL* this, uint32_t edx, IINSTANCE* instance, IDSIMCKT* dsimc
 	lua_load_modules ( this );
 	/* If user uses precompiled device script - don't load external script */
 	lua_getglobal ( this->luactx,"__USE_PRECOMPILED" );
-	if ( lua_isinteger ( this->luactx,lua_gettop ( this->luactx ) ) )
+	if ( '?' == *device_script && lua_isinteger ( this->luactx,lua_gettop ( this->luactx ) ) )
 	{
 		print_info ( this, "%s started [OpenVSM %s, precompiled device script] %s", get_device_id ( this ), __VERSION, LUA_RELEASE );
 	}
