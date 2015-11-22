a221af3298552c09611aa7e467f06659ddb871fd
Pugnator <ookami@mail.ru>
2015-11-22 04:24:36 +0300

    Updated to Lua 5.3.1 Now Lua is built within the project

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 #define FILEVER        1,0,0,0
 #define PRODUCTVER     1,0,0,0
-#define STRFILEVER     "0.3.165\0"
-#define STRPRODUCTVER  "0.3.165\0"
+#define STRFILEVER     "0.3.169\0"
+#define STRPRODUCTVER  "0.3.169\0"
