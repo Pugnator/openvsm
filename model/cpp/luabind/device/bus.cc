8c608d8f43054e83e190fc645ed3b8e50abcb673
Pugnator <ookami@mail.ru>
2024-02-29 23:17:18 +0100

    Removed obsolete files

---
+++ model/cpp/luabind/device/bus.cc
@@ -0,0 +1,7 @@
+#include <log/log.hpp>
+#include "model.hpp"
+#include "lua.hpp"
+
+namespace DeviceSimulator
+{
+}
--- src/modules/module_events.lua
+++
@@ -1,3 +0,0 @@
-function eventsInit()
-	 --for k, v in pairs(device_pins) do info(_G[v.name]) end 
-end
